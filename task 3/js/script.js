304466dbd167caaf930560e9d6880eb232fe9810
Fatma Hossam <hossamfatma565@gmail.com>
2026-08-09 15:09:39 +0300

    update task3

--- task 3/js/script.js
+++ task 3/js/script.js
@@ -1,16 +1,22 @@
 let degree = prompt("Enter your degree: ");     
-if(degree > 90 && degree <= 100){
-    alert("Your grade is A");
-}
-else if(degree > 80 && degree <= 90){
-    alert("Your grade is B");
-}
-else if(degree >= 70 && degree <= 80){
-    alert("Your grade is C");
-}
-else if(degree >= 0 && degree < 70){
-    alert("Your grade is F");
-}
-else{
-    alert("Invalid degree entered. Please enter a number between 0 and 100.");
+if(degree === null || degree.trim() === ""|| isNaN(degree)) {
+    alert("null");
+} else {
+      degree = Number(degree);
+    if (degree <0 || degree > 100) {
+        alert("Please enter a valid degree between 0 and 100.");
+    } else {
+        let grade;
+        
+        if (degree > 90) {
+            grade = "A";
+        } else if (degree >= 80) {
+            grade = "B";
+        } else if (degree >= 70) {
+            grade = "C";
+        } else {
+            grade = "F";
+        }
+        alert("Your grade is: " + grade);
+    }
 }
