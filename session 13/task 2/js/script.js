e2fd4215ac53861110332b933dadef15c4c2232b
Fatma Hossam <hossamfatma565@gmail.com>
2026-08-12 01:05:23 +0300

    task 13

---
+++ session 13/task 2/js/script.js
@@ -0,0 +1,32 @@
+var users = []
+if (document.getElementById('userForm')) {
+    var form = document.getElementById('userForm')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        var obj = {
+            name: e.target[0].value,
+            email: e.target[1].value
+        }
+        users.push(obj)
+        console.table(users)
+        showUser(obj)
+        form.reset()
+    })
+}
+function showUser(user) {
+    var cardsCont = document.getElementById('cards-container')
+
+    var divElement = document.createElement('div')
+    var h4Name = document.createElement('h4')
+    var h4Email = document.createElement('h4')
+
+    h4Name.innerText = "Name : " + user.name
+    h4Email.innerText = "Email : " + user.email
+    
+    divElement.appendChild(h4Name)
+    divElement.appendChild(h4Email)
+
+    divElement.classList.add('alert', 'alert-primary')
+
+    cardsCont.appendChild(divElement)
+}
